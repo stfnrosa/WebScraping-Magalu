2d0f7527953712ec03d9446141488cddaaf4f658
stfnrosa <rosaa.estefanii@gmail.com>
2024-07-13 12:59:16 -0300

    criando diretorio e planilha

--- webscraping.py
+++ webscraping.py
@@ -3,12 +3,15 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import re
+import os
 
 options = Options()
 options.headless = True  # Opcional: executar em modo headless
 driver = webdriver.Firefox(options=options)
 
-base_url = "https://www.magazineluiza.com.br/busca/notebook/?from=clickSuggestion"
+domain = "https://www.magazineluiza.com.br"
+base_url = "https://www.magazineluiza.com.br/busca/notebooks/?from=clickSuggestion&filters=entity---notebook"
 
 products = []
 
@@ -26,15 +29,32 @@ for page_number in range(1, 3):
         product_reviews = item.find('span', class_='sc-epqpcT jdMYPv')
 
         if product_title and product_link:
-            products.append({
-                'title': product_title.text,
-                'link': product_link['href'],
-                'reviews': product_reviews.text if product_reviews else 'No reviews'
-            })
+            reviews_number = None
+            if product_reviews:
+                reviews_number = int(re.findall(r'\(([^)]+)\)', product_reviews.text)[0]) 
 
-driver.quit()
+            if reviews_number:  # Adiciona apenas produtos com avaliações
+                products.append({
+                    'PRODUTO': product_title.text,
+                    'QTD_AVAL': reviews_number,
+                    'URL': domain + product_link['href']
+                })
 
 df = pd.DataFrame(products)
-print(df)
+
+# Criar a pasta Output se não existir
+output_dir = 'Output'
+os.makedirs(output_dir, exist_ok=True)
+
+# Separar notebooks em "Melhores" e "Piores"
+melhores = df[df['QTD_AVAL'] >= 100]
+piores = df[df['QTD_AVAL'] < 100]
+
+# Criar o arquivo Excel com múltiplas abas
+with pd.ExcelWriter(os.path.join(output_dir, 'Notebooks.xlsx'), engine='xlsxwriter') as writer:
+    melhores.to_excel(writer, sheet_name='Melhores', index=False)
+    piores.to_excel(writer, sheet_name='Piores', index=False)
+
+print("Arquivo Excel 'Notebooks.xlsx' criado com sucesso em 'Output'.")
 
         
